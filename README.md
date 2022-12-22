54a9ed938dc6105910cc7e8ae009bc926407d499
MarioMonir <mario3monir@gmail.com>
2022-12-22 17:43:13 +0200

    Inital Commit

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # go-mail-service
+
+#### Project aims to have a simple native golang service ( api server )
+
+---
+
+##### Technologies :
+
+- just golang ( GO )
+
+##### Installation :
+
+- no need for any installation if you already have golang
+
+##### Run :
+
+Api server will run on port 8080
+
+    go run main.go
+
+##### API Documentation
+
+on same url "/" : http://localhost:8080/
+
+---
+
+- Index Home page : [ GET ]
+
+  - serves a welcome html message :
+
+    ```
+    <center>
+    <h1>Welcome to Golang Mailing Service using Gmail !</h1>
+    </center>
+    ```
+
+---
+
+- Send mail : [ POST ]
+  - req body:
+    ```
+    {
+        "to":"<email@exampl.com>",
+        "subject":"email_Subject",
+        "body":"email_body"
+    }
+    ```
+  - serve a json response body
+    ```
+    {
+        "success": "true",
+        "url": "/",
+        "method": "POST",
+        "status": "202",
+        "message": "success to send mail"
+    }
+    ```
+
+---
+
+###### @author MarioMonir
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
-// -------------------------------------------------------
-
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/smtp"
@@ -12,6 +12,8 @@ import (
 
 const (
 	port     = ":8080"
+	host     = "smtp.gmail.com"
+	mailPort = ":587"
 	user     = "techhivedevs@gmail.com"
 	password = "jigteoorzbvvpktc"
 	apiKey   = "mario_secret"
@@ -19,36 +21,136 @@ const (
 
 // -------------------------------------------------------
 
-func sendMail(to string, subject string, body string) error {
-	err := smtp.SendMail("smtp.gmail.com:587",
-		smtp.PlainAuth("", user, password, "smtp.gmail.com"),
-		user,
-		[]string{to},
-		[]byte("To:"+to+"\r\n"+
-			"Subject:"+subject+"\r\n"+
-			"\r\n"+
-			body+"\r\n"),
-	)
-	if err != nil {
-		return err
+// Mail Hanndler Struct
+type Mail struct {
+	To      string
+	Subject string
+	Body    string
+}
+
+// Error Hanlder Struct
+type Error struct {
+	Name    string
+	Code    string
+	Message string
+}
+
+// -------------------------------------------------------
+
+// Send Mail
+// the function sends a mail by gmail to certain email
+// passed as to with subject and body through a struct
+// of type Mail{To, Subject, Body}
+func sendMail(mail Mail) error {
+	if mail.To == "" || mail.Subject == "" || mail.Body == "" {
+		return errors.New(" Mail must have a valid  to, subject, body")
 	}
-	return nil
+	return smtp.SendMail(
+		host+mailPort,
+		smtp.PlainAuth("", user, password, host),
+		user,
+		[]string{mail.To},
+		[]byte(""+
+			"To:"+mail.To+"\r\n"+
+			"Subject:"+mail.Subject+"\r\n\r\n"+
+			mail.Body+"\r\n"),
+	)
 }
 
 // -------------------------------------------------------
 
-func mailHandler() http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		sendMail("mario3monir@gmail.com", "subject mario", "hello mario")
-		w.Write([]byte("Hello World to Golang Mailing Service by Gmail :)"))
-	})
+// Mail Handler
+// the handler aims to act as controller for path POST "/"
+// with a body bind to type Mail and use the sendMail method
+// to send the mail then respons w json response
+func mailHandler(w http.ResponseWriter, r *http.Request) {
+	var mail Mail
+	// Decode Request Body to Struct Mail
+	err := json.NewDecoder(r.Body).Decode(&mail)
+	if err != nil {
+		errorHandler(w, r, Error{Name: "DecodeMailFailure", Code: "400", Message: err.Error()})
+		return
+	}
+	// Sending Mail
+	err = sendMail(mail)
+	if err != nil {
+		errorHandler(w, r, Error{Name: "SendMailFaillure", Code: "500", Message: err.Error()})
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{` +
+		`"success":"true",` +
+		`"url":"` + r.URL.Path + `",` +
+		`"method":"` + r.Method + `",` +
+		`"status":"202",` +
+		`"message": "success to send mail"` +
+		`}`))
 }
 
 // -------------------------------------------------------
 
+// Index Handler
+// acts as a controller for path GET "/" for the app,
+// server a html welcome message to the client
+func indexHanlder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write(
+		[]byte("" +
+			"<center>" +
+			"<h1>Welcome to Golang Mailing Service using Gmail !</h1>" +
+			"</center>"))
+}
+
+// -------------------------------------------------------
+
+// Error Handler
+// Send A Error in json for to the clinet, errors contains
+// success boolean false ,url of request , method of request ,
+// error of type Error{Name, Code, Message}
+func errorHandler(w http.ResponseWriter, r *http.Request, e Error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{` +
+		`"success": "false",` +
+		`"url":"` + r.URL.Path + `",` +
+		`"method":"` + r.Method + `",` +
+		`"status":"` + e.Code + `",` +
+		`"errorName":"` + e.Name + `",` +
+		`"message": "` + e.Message + `"` +
+		`}`))
+}
+
+// -------------------------------------------------------
+
+// Router
+// function aims to return a http.HandlerFunc by routing
+// between if path is GET "/" server indexHandler
+// and if path is POST "/" server mailHandler
+// else will serve the errorHandler 404 NotFound
+func router() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" && r.Method == "GET" {
+			indexHanlder(w, r)
+			return
+		}
+		if r.URL.Path == "/" && r.Method == "POST" {
+			mailHandler(w, r)
+			return
+		}
+
+		errorHandler(w, r,
+			Error{Name: "NotFound", Code: "404",
+				Message: "url path method is not found"})
+	}
+}
+
+// -------------------------------------------------------
+
+// Main
+// Entry Point for app and the server launcher
 func main() {
 	fmt.Println("Server is Listintg on port", port)
-	if err := http.ListenAndServe(port, mailHandler()); err != nil {
+	http.HandleFunc("/", router())
+	if err := http.ListenAndServe(port, nil); err != nil {
 		panic(err)
 	}
 }
